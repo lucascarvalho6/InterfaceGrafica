5b4e871fe7f79eecad58faade9fb44fbe6c3c54b
lucascarvalho6 <lucas.carvalho269@etec.sp.gov.br>
2023-05-20 13:16:38 -0300

    Aula de PAM - Sábado (20/05)

---
+++ interfaceGrafica/app/src/main/java/com/example/interfacegrafica/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.interfacegrafica
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.interfacegrafica.ui.theme.InterfaceGraficaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    var contador by remember{
+        mutableStateOf(0)
+    }
+    Column() {
+        Button(onClick = {
+            contador = contador + 1
+            Log.i("##### NOSSO_LOG", "Vlr do Contador = $contador")
+        }) {
+            Text(text = "INCREMENTA CONTADOR")
+        }
+        Text(text = "Vlr do Contador = $contador")
+    }
+    Log.i("##### NOSSO_LOG", "Vlr do Contador = $contador")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MinhaPreview() {
+    MainScreen()
+}
---
+++ interfaceGrafica/app/src/main/java/com/example/interfacegrafica/MinhaViewModelBemSimples.kt
@@ -0,0 +1,15 @@
+package com.example.interfacegrafica
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MinhaViewModelBemSimples: ViewModel() {
+
+    private val _contadorDaViewModel = MutableStateFlow(0)
+    val contadprDaViewPublico: StateFlow<Int> get() = _contadorDaViewModel
+
+    fun incremenaContador(){
+        _contadorDaViewModel.value = _contadorDaViewModel.value + 1
+    }
+}
